3741204fe82ee66bdf3f4d635eb7b09b9a735eab
djigui <84602700+Djigui@users.noreply.github.com>
2021-09-07 15:51:48 +0200

    sono esercitazione, sia il back-end, front-end

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-console.log('hello world');
+
+/*const mariag =(numer, adama) => {
+    let x = [];
+    let z = 2;
+   while(x.length <= adama){
+if(z % adama == 0) {
+    x.push(z);
+}
+z++;
+   }
+   return x;
+}
+
+
+
+console.log(mariag(6, 200)+ '  hhhhh');
+
+
+for(let i = 2; i < 20; i++){
+    if(i ===19){
+    console.log('ciao, i come stai?')
+    } 
+
+} 
+
+const numer = (num, num3) => {
+let ouma = ['ada', 'gg', 'gasfs', 'ffd' ];
+
+
+
+}
+console.log(numer( 100));
+console.log();
+
+}
+console.log(persona.nome)
+console.log(persona.interessi.length);
+persona.interessi.forEach(element =>{
+  console.log(element);
+});
+persona.interessi.push ='barista'
+console.log(persona)
+
+const persona = (n1, limit) => {
+  valia =[];
+  bamimi =2;
+  while(valia.length <= limit){
+    if(bamimi % n1 === 0){
+      valia.push(bamimi);
+
+    }
+    bamimi++;
+  }
+  return valia;
+}
+console.log(persona(200, 2)+  'ciaooooooooooooooo ');
+
+let x =20;
+let y =2
+if(y ==x){
+  console.log('ciaoooo x');
+
+} if(x <  y){
+  console.log('sambesambe');
+
+} if(x = 0){
+  console.log('bboooooooooo');
+}else{
+  console.log('the batle lid, and the car door, else the vampire hunthe')
+}
+*/
+const button1 = document.getElementById("button1");
+
+console.log(button1 + 'ciaoo')
